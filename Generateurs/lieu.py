11b828e584450a0dcf122258861644ec75a3fba2
LSchnuriger <loic.cham74@gmail.com>
2021-11-18 22:31:25 +0100

    Update lieu.py

--- Generateurs/lieu.py
+++ Generateurs/lieu.py
@@ -2,6 +2,7 @@ import pandas as pd
 import random
 
 
+
 NOM_DE_LA_TABLE = "lieu"
 NB_LIEU = 100
 
@@ -16,14 +17,18 @@ cp = df_ville[8]
 
 file = open(NOM_DE_LA_TABLE + ".txt", "w") #"x" si le fichier existe pas
 
+moy = 2000
+sigma = 800
 
-
-for i in range(0, NB_ADRESSE): 
+for i in range(0, NB_LIEU): 
     n = random.randint(0, len(df_rue)-1)
     ville = v[n]
     codeP = cp[n]
-    j = random.randint(1, 1200)
-    file.write("INSERT INTO " + NOM_DE_LA_TABLE + " VALUES (" + str(i) + ",'" + ville + "', " + str(j) + ", '" + random.choice(r) + "', '" + codeP + "');\n")
+    revenu = random.randint(1200, 10000)
+    Population = round(random.gauss(100, 50))
+    if Population <= 0:
+        Population = 10
+    file.write("INSERT INTO " + NOM_DE_LA_TABLE + " VALUES (" + str(i) + ", '" + codeP + "','" + ville + "', '" + random.choice(r) + "','" + str(revenu) + "','" + str(Population) + "');\n")
 
 
 file.close()
