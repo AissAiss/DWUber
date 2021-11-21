ad8903196d9abb83bbd231121163c5157630d5f9
LSchnuriger <loic.cham74@gmail.com>
2021-11-21 12:07:37 +0100

    Create VirtualViews.sql

---
+++ VirtualViews.sql
@@ -0,0 +1,12 @@
+create view ride_date as select * from dateC;
+create view search_date as select * from dateC;
+
+create view search_time as select * from timeOfDay;
+create view order_time as select * from timeOfDay;
+create view dep_time as select * from timeOfDay;
+create view arr_time as select * from timeOfDay;
+
+
+create view search_district as select * from district;
+create view dep_district as select * from district;
+create view arr_district as select * from district;
