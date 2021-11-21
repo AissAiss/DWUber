175470791cb9d52cc63be9f90225a71639747d9a
LSchnuriger <loic.cham74@gmail.com>
2021-11-21 12:12:39 +0100

    Update DWmain.sql

--- DWmain.sql
+++ DWmain.sql
@@ -107,3 +107,33 @@ alter table ride add foreign key(idArrTime) references timeOfDay(idTime);
 alter table search add foreign key(idDateSearch) references dateC(idDate);
 alter table search add foreign key(idTimeSearch) references timeOfDay(idTime);
 alter table search add foreign key(idDistrict) references district(idDistrict);
+
+
+-- Destruction et création des vues virtuelles
+
+drop view ride_date;
+drop view search_date;
+
+drop view search_time;
+drop view order_time;
+drop view dep_time;
+drop view arr_time;
+
+drop view search_district;
+drop view dep_district;
+drop view arr_district;
+
+
+
+create view ride_date as select * from dateC;
+create view search_date as select * from dateC;
+
+create view search_time as select * from timeOfDay;
+create view order_time as select * from timeOfDay;
+create view dep_time as select * from timeOfDay;
+create view arr_time as select * from timeOfDay;
+
+
+create view search_district as select * from district;
+create view dep_district as select * from district;
+create view arr_district as select * from district;
