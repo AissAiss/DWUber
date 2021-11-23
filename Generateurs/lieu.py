9409dd7fa8ab78f1614ad698008c0c89eb691b6f
LSchnuriger <loic.cham74@gmail.com>
2021-11-23 10:59:11 +0100

    Update lieu.py

--- Generateurs/lieu.py
+++ Generateurs/lieu.py
@@ -3,32 +3,24 @@ import random
 
 
 
-NOM_DE_LA_TABLE = "lieu"
-NB_LIEU = 100
-
-df_rue = pd.read_csv('rue.csv', sep=";", encoding='utf-8')
-df_ville = pd.read_csv('ville.csv',encoding='latin-1', usecols=[3,8], header=None ,engine='python')
-
-r = df_rue['rue']
-
-v = df_ville[3]
-cp = df_ville[8]
+NOM_DE_LA_TABLE = "district"
+District = ["HOPITAUX-FACULTES", "MOSSON" ,"LES CEVENNES","PORT MARIANNE","MONTPELLIER CENTRE","CROIX D ARGENT","PRES D ARENE"]
+Revenu = [1800, 1200, 1250, 2000, 2200, 1500, 1700]
+Population = [15000, 30000, 30000, 10000, 8000, 30000, 25000]
+v = "Montpellier"
 
 
 file = open(NOM_DE_LA_TABLE + ".txt", "w") #"x" si le fichier existe pas
 
-moy = 2000
-sigma = 800
+codeP = 34000
 
-for i in range(0, NB_LIEU): 
-    n = random.randint(0, len(df_rue)-1)
-    ville = v[n]
-    codeP = cp[n]
-    revenu = random.randint(1200, 10000)
-    Population = round(random.gauss(100, 50))
-    if Population <= 0:
-        Population = 10
-    file.write("INSERT INTO " + NOM_DE_LA_TABLE + " VALUES (" + str(i) + ", '" + codeP + "','" + ville + "', '" + random.choice(r) + "','" + str(revenu) + "','" + str(Population) + "');\n")
-
+for i in range(0, len(District)): 
+    
+    ville = District[i]
+    
+    
+    file.write("INSERT INTO " + NOM_DE_LA_TABLE + " VALUES (" + str(i) + ", '" + str(codeP) + "','" + v + "', '" + District[i] + "','" + str(Revenu[i]) + "','" + str(Population[i]) + "');\n")
+    codeP = codeP+10
 
 file.close()
+
