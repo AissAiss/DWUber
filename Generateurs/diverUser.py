3dec6dfddad0d840c8a40233091dd43cc4579c49
LSchnuriger <loic.cham74@gmail.com>
2021-11-23 12:09:23 +0100

    Update diverUser.py

--- Generateurs/diverUser.py
+++ Generateurs/diverUser.py
@@ -14,16 +14,16 @@ DATE = "dateC"
 UTILISATEUR = "userUber"
 COURSE = "ride"
 RECHERCHE = "search"
-NOMBRE_DE_USER = 15000
+NOMBRE_DE_USER = 5000
 NOMBRE_DE_DRIVER = 250
 
-file = open("DUGen" + ".txt", "x") #"x" si le fichier existe pas
+file = open("DUGen" + ".txt", "w") #"x" si le fichier existe pas
 
 for i in range(0, NOMBRE_DE_USER): 
-    file.write("INSERT INTO " + UTILISATEUR + " VALUES (" + str(i)+ ",'" + random.choice(p) + "', '" + random.choice(n) + "', " + str(random.randrange(15, 75)) + ");\n")
+    file.write("INSERT INTO " + UTILISATEUR + " VALUES (" + str(i)+ ",'" + random.choice(p) + "', '" + random.choice(n) + "', " + str(random.randint(15, 75)) + ");\n")
     
     
-for i in range(0, NOMBRE_DE_DRIVER): 
-    file.write("INSERT INTO " + CHAUFFEUR + " VALUES (" + str(i)+ ",'" + random.choice(p) + "', '" + random.choice(n) + "');\n")
+for j in range(0, NOMBRE_DE_DRIVER): 
+    file.write("INSERT INTO " + CHAUFFEUR + " VALUES (" + str(j)+ ",'" + random.choice(p) + "', '" + random.choice(n) + "');\n")
     
 file.close()
